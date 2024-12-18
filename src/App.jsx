e2088d87dd874d5bb442c3aca385bb8264764391
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-12-18 17:10:04 +0100

    test pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
-import HomePage from './Pages/HomePage'
+import HomePage from './Pages/HomePage.jsx';
 import '../src/Style/main.scss';
 
 function App() {
-
   return (
-    <Router>
+    <>
       <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+      <HomePage />
       <Footer />
-    </Router>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
