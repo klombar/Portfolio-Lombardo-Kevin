aaa696e172da92774d5640991a0015841fa46d70
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-12-18 17:39:15 +0100

    test pages

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 
 function Nav() {
-   return (
-     <nav className="nav">
-       <ul className="liste">
-         <li><a href="#accueil">Home</a></li>
-         <li><a href="#competences">Compétences techniques</a></li>
-         <li><a href="#projets">Projets</a></li>
-         <li>Contact</li>
-       </ul>
-     </nav>
-   );
- }
+  return (
+    <nav className="nav">
+      <ul className="liste">
+        <li><a href="#accueil">Home</a></li>
+        <li><a href="#competences">Compétences techniques</a></li>
+        <li><a href="#projets">Projets</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
 
- export default Nav;
+export default Nav;
