b99044c1a568309e56d71989e5a552aa134c8999
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-12-18 17:15:35 +0100

    test pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Lombardo-Kevin/',
-  build: {
-    minify: 'esbuild',
-  },
+  base: 'https://github.com/klombar/Portfolio-Lombardo-Kevin/',
 })
