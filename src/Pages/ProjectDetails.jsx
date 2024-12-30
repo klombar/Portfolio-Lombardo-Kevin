8ebbaf2186e4ed238c48168b2b6c927d2e5599d3
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-12-30 11:42:57 +0100

    test detailsProject

--- src/Pages/ProjectDetails.jsx
+++ src/Pages/ProjectDetails.jsx
@@ -3,55 +3,42 @@ import { useEffect, useState } from 'react';
 import Button from '../Components/UI/Button';
 
 function ProjectDetails() {
-
-  const { projectId } = useParams(); 
+  const { projectId } = useParams();
   const [project, setProject] = useState(null);
 
   useEffect(() => {
-   // Utiliser process.env.PUBLIC_URL pour obtenir le chemin correct en fonction de l'environnement
-   fetch('/carrouselData.json')
-     .then(response => response.json())
-     .then(data => {
-       const foundProject = data.find(p => p.id === projectId);
-       setProject(foundProject);
-     })
-     .catch(error => console.error('Error fetching project data:', error));
- }, [projectId]);
- 
- if (!project) {
-   return <div>Chargement...</div>;
- }
+    fetch('/carrouselData.json')
+      .then(response => response.json())
+      .then(data => {
+        const foundProject = data.find(p => p.id === projectId);
+        setProject(foundProject);
+      })
+      .catch(error => console.error('Error fetching project data:', error));
+  }, [projectId]);
 
-   return (
-      <>
-       <div className="project-detail">
-      <h1>Visuel Projet {project.title}</h1>
-      <div className='projectDetail_functionnality'>
-         <div className='projectDetail_functionnality_image_container'>
-            <div className='projectDetail_functionnality_image'>
-               <img src={project.image} alt={project.title} />
+  if (!project) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <div className="project-detail">
+      <h1>Visuels du Projet {project.title}</h1>
+
+        <div className="projectDetail_functionnality_image_container">
+          {Object.values(project.projectDetailsImages).map((imageUrl, index) => (
+            <div className="projectDetail_functionnality_image" key={index}>
+              <img src={imageUrl} alt={`${project.title} image ${index + 1}`} />
             </div>
-            <div className='projectDetail_functionnality_image'>
-               <img src={project.image} alt={project.title} />
-            </div>
-         </div>
-         <div className='projectDetail_functionnality_image_container'>
-            <div className='projectDetail_functionnality_image'>
-               <img src={project.image} alt={project.title} />
-            </div>
-            <div className='projectDetail_functionnality_image'>
-               <img src={project.image} alt={project.title} />
-            </div>
-         </div>
-      </div>
-      <div className='projectDetail_button_container'>
-         <Button link={project.link} classname='carrousel_link_button'>
-            Voir sur GitHub
-         </Button>
+          ))}
+        </div>
+
+      <div className="projectDetail_button_container">
+        <Button link={project.link} classname="carrousel_link_button">
+          Voir sur GitHub
+        </Button>
       </div>
     </div>
-      </>
-   )
+  );
 }
 
 export default ProjectDetails;
