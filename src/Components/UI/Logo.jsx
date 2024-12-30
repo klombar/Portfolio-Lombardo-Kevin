23844b7f29a5cbce167da819dab016013a5f69e7
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-12-30 09:43:44 +0100

    ajout de Link sur le logo pour revenir a la homepage

--- src/Components/UI/Logo.jsx
+++ src/Components/UI/Logo.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import proptypes from 'prop-types';
 
 function Logo({className, src, alt}) {
   return (
-    <>
-      <img src={src} alt={alt} className={className} />
-    </>
+      <Link to="/">
+        <img src={src} alt={alt} className={className}/>
+      </Link>
   );
 }
 
