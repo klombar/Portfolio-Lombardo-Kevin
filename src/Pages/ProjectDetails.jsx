9629d439edbb92fb6a822a5afadcbf2751b37fc3
Lombardo Kevin <kevinlombardo30@gmail.com>
2024-12-31 17:40:05 +0100

    ajout d'une condition et d'un bouton pour pouvoir ouvrir le fichier d'optimisation du projet Nina Carducci

--- src/Pages/ProjectDetails.jsx
+++ src/Pages/ProjectDetails.jsx
@@ -24,18 +24,28 @@ function ProjectDetails() {
     <div className="project-detail">
       <h1>Visuels du Projet {project.title}</h1>
 
-        <div className="projectDetail_functionnality_image_container">
-          {Object.values(project.projectDetailsImages).map((imageUrl, index) => (
-            <div className="projectDetail_functionnality_image" key={index}>
-              <img src={imageUrl} alt={`${project.title} image ${index + 1}`} loading='lazy' />
-            </div>
-          ))}
-        </div>
+      {/* Affichage des images de détail du projet */}
+      <div className="projectDetail_functionnality_image_container">
+        {Object.values(project.projectDetailsImages).map((imageUrl, index) => (
+          <div className="projectDetail_functionnality_image" key={index}>
+            <img src={imageUrl} alt={`${project.title} image ${index + 1}`} loading="lazy" />
+          </div>
+        ))}
+      </div>
 
+      {/* Affichage des boutons */}
       <div className="projectDetail_button_container">
+        {/* Bouton GitHub */}
         <Button link={project.link} classname="carrousel_link_button">
           Voir sur GitHub
         </Button>
+
+        {/* Vérification de la présence du rapport d'optimisation */}
+        {project.documents && project.documents.trim() !== "" && (
+          <Button link={project.documents} classname="carrousel_link_button">
+            Voir le rapport d&apos;optimisation lié à ce projet.
+          </Button>
+        )}
       </div>
     </div>
   );
