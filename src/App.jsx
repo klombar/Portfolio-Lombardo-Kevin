29172a5024351debb65be590391e0af41b2a3d53
Lombardo Kevin <kevinlombardo30@gmail.com>
2025-01-01 13:30:40 +0100

    ajout de la page error404

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './Components/Layout/Footer/Footer.jsx';
 import HomePage from './Pages/HomePage.jsx';
 import ProjectDetails from './Pages/ProjectDetails.jsx';
 import ArrowToTop from './Components/UI/ArrowToTop.jsx';
+import Error404 from './Pages/Error404.jsx';
 import '../src/Style/main.scss';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/project/:projectId" element={<ProjectDetails />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
       <Footer />
       <ArrowToTop />
---
+++ src/Pages/Error404.jsx
@@ -0,0 +1,13 @@
+function Error404() {
+  return (
+    <div className="error404">
+      <div className="error404_title">404: Not Found</div>
+      <div className="error404_description">
+         La page que vous avez demandée n&apos;a pas été trouvée. 
+         Veuillez cliquer sur le logo afin de revenir a la page d'accueil.
+      </div>
+    </div>
+  );
+}
+
+export default Error404;
